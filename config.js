bebf16c195665ce3e1f4af5f1dbf0cc449994487
y-redhat <238958916+y-redhat@users.noreply.github.com>
2026-02-08 22:11:40 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export function loadConfig() {
     
     return {
         supabaseUrl: 'https://figycpclqkoarpkkvwxz.supabase.co',
-        supabaseKey: window.SUPABASE_KEY || 'sb_publishable_j9KTa-6LI8llwinlZJI9ww_0NnAq714'
+        supabaseKey: window.SUPABASE_KEY || ""
     };
 }
 
